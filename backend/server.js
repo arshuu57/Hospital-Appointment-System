b28ead8ab95bcd35307a35270923728e6bdf99d9
Arshad khan <arshadkhan73316@gmail.com>
2025-09-27 21:23:51 +0530

    deploy erorr fixed

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,16 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const appointmentRoutes = require('./routes/appointments');
-
-app.get("/", (req, res) => {
-  res.send("✅ API is working");
-});
-
-
 const app = express();
+
+
+
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send(" API is working");
+});
 
 mongoose.connect(process.env.MONGO_URI, {
 
